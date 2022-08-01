dc1b6b3fc776f2d4a63afe728cf69a716dc190d6
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2022-08-01 17:46:34 +0900

    swea13736 solved

---
+++ problems/swea13736/13736.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+long long power_2(int sumAB, int K);
+
+int main(void) {
+    long long T, A, B, K, pow, minAB;
+    cin >> T;
+    
+    for(int i = 0; i < T; i++) {
+        cin >> A >> B >> K;
+        minAB = min(A, B);
+        pow = power_2(A+B, K);
+        minAB = (minAB * pow) % (A+B);
+        cout << "#" << i+1 << " " << min(minAB, A+B-minAB) <<  "\n";
+    }
+    
+    return 0;
+}
+
+long long power_2(int sumAB, int K) {
+    if(K == 0) {
+        return 1;
+    }
+    long long half, mult;
+    half = power_2(sumAB, K/2);
+    mult = (half * half) % sumAB;
+    if(K % 2 == 0) {
+        return mult;
+    }
+    else {
+        mult = (2 * mult) % sumAB;
+        return mult;
+    }
+}
+
