af9be874aa3aff8cda855a7102bb37e21c256cb8
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2022-08-02 20:51:07 +0900

    swea 10507 solved

---
+++ problems/swea10507/10507.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+    int T, N, P, n, p, max_days;
+    queue<int> Q;
+    cin >> T;
+    
+    for(int t = 0; t < T; t++) {
+        Q = queue<int>();
+        cin >> N >> P;
+        max_days = P;
+        
+        for(int i = 0; i < N; i++) {
+            cin >> n;
+            if(Q.size() == 0) {
+                Q.push(n);
+                p = P;
+            }
+            else {
+                if(n - Q.back() - 1 > P) {
+                    Q = queue<int>();
+                    Q.push(n);
+                    p = P;
+                }
+                else {
+                    int front;
+                    while(p < n - Q.back() - 1) {
+                        front = Q.front();
+                        Q.pop();
+                        p += Q.front() - front - 1;
+                    }
+                    p -= n - Q.back() - 1;
+                    Q.push(n);
+                }
+            }
+            max_days = max(max_days, (int)Q.size() + P);
+        }
+        cout << "#" << t+1 << " " << max_days << "\n";
+    }
+    return 0;
+}
+
